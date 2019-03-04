d4f3d4480d0fd4d08a83a64c923e304d5f114129
vatojavier <antonioj.casado@estudiante.uam.es>
2019-03-04 13:53:40 +0100

    added http filter

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
-     scapy.sniff(iface=interface, store=False, prn=process_packet)
+     scapy.sniff(iface=interface, store=False, prn=process_packet, filter="tcp")
 
 #process the packet received by scapy.sniff called by prn
 def process_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
 
 sniff("wlp3s0")

